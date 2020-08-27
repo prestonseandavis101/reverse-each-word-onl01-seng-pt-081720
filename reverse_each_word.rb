4b4da5cffa40b8d05ef3cad675a881fba266b28b
Preston Davis <prestonseandavis101@yahoo.com>
2020-08-27 03:45:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,6 +4,5 @@ def reverse_each_word(sentence)
   array_sentence << sentence.split
   array_sentence.each do |string|
     new_sentence << string.reverse
-end
-new_sentence
+new_sentence.join(" ")
 end

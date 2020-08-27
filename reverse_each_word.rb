f7f9c42e086708f17f574a655ff383530256937d
Preston Davis <prestonseandavis101@yahoo.com>
2020-08-27 03:50:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   original_array = string.split(" ")
   return_array = []
-  original_array.each do|string|
+  original_array.collect do|string|
     return_array << string.reverse
   end
   return_array.join(" ")

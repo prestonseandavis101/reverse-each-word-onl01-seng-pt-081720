52918a78626694cfa3bb3dbff8ae89779af56b91
Preston Davis <prestonseandavis101@yahoo.com>
2020-08-27 03:44:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,9 @@
-require "pry"
 def reverse_each_word(sentence)
   array_sentence = []
   new_sentence = []
   array_sentence << sentence.split
   array_sentence.each do |string|
     new_sentence << string.reverse
-    binding.pry
 end
 new_sentence
 end
